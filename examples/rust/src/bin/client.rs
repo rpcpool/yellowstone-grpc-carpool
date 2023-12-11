0b7a175d9239c0d7889bae3a238015fa6b3a02e4
Kirill Fomichev <fanatid@ya.ru>
2023-12-11 12:23:45 -0500

    client: print only slots

--- examples/rust/src/bin/client.rs
+++ examples/rust/src/bin/client.rs
@@ -531,27 +531,31 @@ async fn geyser_subscribe(
     while let Some(message) = stream.next().await {
         match message {
             Ok(msg) => {
-                #[allow(clippy::single_match)]
-                match msg.update_oneof {
-                    Some(UpdateOneof::Account(account)) => {
-                        let account: AccountPretty = account.into();
-                        info!(
-                            "new account update: filters {:?}, account: {:#?}",
-                            msg.filters, account
-                        );
-                        continue;
-                    }
-                    Some(UpdateOneof::Transaction(tx)) => {
-                        let tx: TransactionPretty = tx.into();
-                        info!(
-                            "new transaction update: filters {:?}, transaction: {:#?}",
-                            msg.filters, tx
-                        );
-                        continue;
-                    }
-                    _ => {}
+                // #[allow(clippy::single_match)]
+                // match msg.update_oneof {
+                //     Some(UpdateOneof::Account(account)) => {
+                //         let account: AccountPretty = account.into();
+                //         info!(
+                //             "new account update: filters {:?}, account: {:#?}",
+                //             msg.filters, account
+                //         );
+                //         continue;
+                //     }
+                //     Some(UpdateOneof::Transaction(tx)) => {
+                //         let tx: TransactionPretty = tx.into();
+                //         info!(
+                //             "new transaction update: filters {:?}, transaction: {:#?}",
+                //             msg.filters, tx
+                //         );
+                //         continue;
+                //     }
+                //     _ => {}
+                // }
+                // info!("new message: {msg:?}")
+
+                if let Some(UpdateOneof::Slot(msg)) = msg.update_oneof {
+                    info!("new slot message: {msg:?}");
                 }
-                info!("new message: {msg:?}")
             }
             Err(error) => {
                 error!("error: {error:?}");
